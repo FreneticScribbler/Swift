b4f502c0875947e881153479c1ab479bb5103cdb
Paint_Ninja <PaintNinja@users.noreply.github.com>
2019-03-27 14:16:32 +0000

    Implement high refresh rate support, closes #7. And more.
    Implement high refresh rate support, closes #7.
    Commented the code.
    Preparation work for multi-pass checks (#9).
    Change some of the default config settings to facilitate testing.

--- src/main/java/com/aronajones/swift/Swift.java
+++ src/main/java/com/aronajones/swift/Swift.java
@@ -15,16 +15,18 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.config.Configuration;
+import org.lwjgl.opengl.Display;
 
 @Mod(modid = Swift.MODID, name = Swift.NAME, version = Swift.VERSION, canBeDeactivated = false)
 public class Swift {
 
 	public static final String MODID = "swift";
 	public static final String NAME = "Swift";
-	public static final String VERSION = "0.0.4.1";
+	public static final String VERSION = "0.0.4.2";
 
 	public static int chunkUpdates, ticksExisted, ticksBetweenRun;
 	public static int cooldownTicks;
+	public static int refreshRate;
 	public static Value[] lowers = new Value[0];
 	public static Value[] uppers = new Value[0];
 
@@ -42,28 +44,32 @@ public class Swift {
 		config.load();
 		chunkUpdates = config.getInt("chunkUpdates", Configuration.CATEGORY_GENERAL, 15, 0, Integer.MAX_VALUE,
 				"Commands will not run if chunk updates are greater than or equal to this value.");
-		ticksExisted = config.getInt("ticksExisted", Configuration.CATEGORY_GENERAL, 20000, 0, Integer.MAX_VALUE,
+		ticksExisted = config.getInt("ticksExisted", Configuration.CATEGORY_GENERAL, 1200, 0, Integer.MAX_VALUE,
 				"Commands will not run if the player has been in the world for less than this value, given in ticks.");
 		ticksBetweenRun = config.getInt("ticksBetweenRun", Configuration.CATEGORY_GENERAL, 60, 0, Integer.MAX_VALUE,
 				"The frequency with which the check runs.");
 		cooldownTicks = config.getInt("cooldownTicks", Configuration.CATEGORY_GENERAL, 120, 0, Integer.MAX_VALUE,
 				"Cooldown between running of commands/warnings.");
 		int[] lowerFPSValues = config
-				.get(Configuration.CATEGORY_GENERAL, "lowerFPSValues", new int[] {30, 60, 25},
+				.get(Configuration.CATEGORY_GENERAL, "lowerFPSValues", new int[]{30, 60, 25},
 						"Values at which to run commands/give warnings. If the string is empty it will be safely ignored.")
 				.getIntList();
 		String[] lowerCommands = config.getStringList("commandsAtLowerFPSValues", Configuration.CATEGORY_GENERAL,
-				new String[] {"", "", "test"}, "Must be in the same order as FPS values.");
+				new String[]{"", "", ""}, "Must be in the same order as FPS values.");
 		String[] lowerWarnings = config.getStringList("warningsAtLowerFPSValues", Configuration.CATEGORY_GENERAL,
-				new String[] {"Under 30.", "Under 60.", "test"}, "Must be in the same order as FPS values.");
+				new String[]{"Under 30.", "Under 60.", "Under 25"}, "Must be in the same order as FPS values.");
 		int[] upperFPSValues = config
-				.get(Configuration.CATEGORY_GENERAL, "upperFPSValues", new int[] {10},
+				.get(Configuration.CATEGORY_GENERAL, "upperFPSValues", new int[]{40, 100, 120},
 						"Values at which to run commands/give warnings. If the string is empty it will be safely ignored.")
 				.getIntList();
 		String[] upperCommands = config.getStringList("commandsAtUpperFPSValues", Configuration.CATEGORY_GENERAL,
-				new String[] {""}, "Must be in the same order as FPS values.");
+				new String[]{"", "", ""}, "Must be in the same order as FPS values.");
 		String[] upperWarnings = config.getStringList("warningsAtUpperFPSValues", Configuration.CATEGORY_GENERAL,
-				new String[] {"Over 100."}, "Must be in the same order as FPS values.");
+				new String[]{"Over 40.", "Over 100.", "Over 120."}, "Must be in the same order as FPS values.");
+		boolean[] upperFPSValuesOverrides = config
+				.get(Configuration.CATEGORY_GENERAL, "upperFPSValuesOverrides", new boolean[]{false, false, false},
+						"Whether or not to ignore the client's refresh rate when running a corresponding upperFPSValue. Set to true to ignore, that is, force the execution of any commands and warnings associated with the corresponding upperFPSValue, regardless of the client's refresh rate.")
+				.getBooleanList();
 		if(config.hasChanged())
 			config.save();
 
@@ -72,15 +78,16 @@ public class Swift {
 		assert lowerCommands.length == lowerWarnings.length;
 		assert upperFPSValues.length == upperCommands.length;
 		assert upperCommands.length == upperWarnings.length;
+		assert upperFPSValuesOverrides.length == upperFPSValues.length;
 
 		ArrayList<Value> lowers = new ArrayList<Value>();
 		ArrayList<Value> uppers = new ArrayList<Value>();
 
 		for(int i = 0; i < lowerFPSValues.length; i++)
-			lowers.add(new Value(lowerFPSValues[i], lowerCommands[i], lowerWarnings[i]));
+			lowers.add(new Value(lowerFPSValues[i], lowerCommands[i], lowerWarnings[i], false));
 
 		for(int i = 0; i < upperFPSValues.length; i++)
-			uppers.add(new Value(upperFPSValues[i], upperCommands[i], upperWarnings[i]));
+			uppers.add(new Value(upperFPSValues[i], upperCommands[i], upperWarnings[i], upperFPSValuesOverrides[i]));
 
 		lowers.sort(new ComparatorValue());
 		uppers.sort(new ComparatorValue());
@@ -90,17 +97,26 @@ public class Swift {
 
 		FMLCommonHandler.instance().bus().register(new SwiftEventHandler());
 		ClientCommandHandler.instance.registerCommand(new ProfileCommand());
+
+		// Get the refresh rate of the player's screen (only checked at launch as LWJGL doesn't update this value after the game is launched)
+		if(Display.isFullscreen()) {
+			refreshRate = Display.getDisplayMode().getFrequency(); // get the fullscreen mode refresh rate
+		} else {
+			refreshRate = Display.getDesktopDisplayMode().getFrequency(); // get the desktop mode refresh rate
+		}
 	}
 
 	class Value {
 		public int fps;
 		public String command;
 		public String warning;
+		public boolean overrides;
 
-		public Value(int fps, String command, String warning) {
+		public Value(int fps, String command, String warning, boolean overrides) {
 			this.fps = fps;
 			this.command = command;
 			this.warning = warning;
+			this.overrides = overrides;
 		}
 	}
 
--- src/main/java/com/aronajones/swift/SwiftEventHandler.java
+++ src/main/java/com/aronajones/swift/SwiftEventHandler.java
@@ -14,57 +14,131 @@ public class SwiftEventHandler {
 	int ticks = 0;
 	int cooldown = 0;
 	int lastLoadedProfile = -1;
+	int secondLastLoadedProfile = -1;
+
+	int refreshRate = 0;
 
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event) {
 		if(event.side.isClient()) {
 			if(cooldown > 0)
+				// Deduct the cooldown on every tick
 				cooldown--;
 
+			// If it's past the cooldown time and the tick interval time
 			if(ticks >= Swift.ticksBetweenRun && cooldown == 0) {
+				// reset the ticks interval
 				ticks = 0;
+
+				// for running any commands associated to profiles later on
 				EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+
+				// grab all the details from the F3 debug screen
 				String[] debug = Minecraft.getMinecraft().debug.split(" ");
-				int fps = Integer.parseInt(debug[0]);
-				int chunkUpdates = Integer.parseInt(debug[2]);
+				int fps = Integer.parseInt(debug[0]); // grab the current FPS as shown on the debug screen
+				int chunkUpdates = Integer.parseInt(debug[2]); // do the same for chunk updates
+
+				// Optifine tends to attempt to keep the chunk updates to at least the framerate.
 				if(FMLClientHandler.instance().hasOptifine()) {
-					chunkUpdates = chunkUpdates - fps;
+					chunkUpdates = chunkUpdates - fps; // Take this into account for consistent behaviour.
 				}
+
+				// Don't run Swift when chunks are still being loaded and/or the player has recently entered the world
 				if(chunkUpdates >= Swift.chunkUpdates || player.ticksExisted < Swift.ticksExisted)
 					return;
+
+				// for each entry in the "lowers" profile entries config
 				for(int i = 0; i < Swift.lowers.length; i++) {
+
+					// if the framerate is below the "lowers" profile entry
 					if(fps < Swift.lowers[i].fps) {
+
+						// if the profile we're about to load isn't already loaded
 						if(i != lastLoadedProfile) {
+
+							// if a command for the profile has been specified in the config, run it
 							if(!Swift.lowers[i].command.isEmpty()) {
 								ClientCommandHandler.instance.executeCommand(player, Swift.lowers[i].command);
 							}
+
+							// if a warning for the profile has been specified in the config, print it out to the chat
 							if(!Swift.lowers[i].warning.isEmpty()) {
 								player.addChatMessage(new ChatComponentText(Swift.lowers[i].warning));
 							}
 						}
+
+						// reset the cooldown timer to the value specified in the config
 						cooldown = Swift.cooldownTicks;
+
+						// set the secondLastLoadedProfile to lastLoadedProfile
+						secondLastLoadedProfile = lastLoadedProfile;
+
+						// set the lastLoadedProfile to the current one
 						lastLoadedProfile = i;
+
+						// done
 						break;
 					}
 				}
+
+				// for each entry in the "uppers" profile entries config
 				for(int i = 0; i < Swift.uppers.length; i++) {
-					if(fps > Swift.uppers[i].fps) {
+					/*player.addChatMessage(new ChatComponentText("Swift debug: current uppers: " + Swift.uppers[i].warning));
+					player.addChatMessage(new ChatComponentText("Uppers length: " + Swift.uppers.length));
+					player.addChatMessage(new ChatComponentText("Swift debug: Math.ceil(refreshRate * 0.8) = " + Math.ceil(refreshRate * 0.8)));*/
+
+                    // refreshRate * 0.6:
+					// 144hz = 86.4
+					// 120hz = 72
+					// 75hz = 45
+					// 60hz = 36
+
+                    // refreshRate * 0.7:
+					// 144hz = 100.8
+					// 120hz = 84
+					// 75hz = 52.5
+					// 60hz = 42
+
+					// refreshRate * 0.8:
+					// 144hz = 115.2
+					// 120hz = 96
+					// 75hz = 60
+					// 60hz = 48
+
+					// if the framerate is above the "uppers" profile entry and above (or equal to) 80% of the monitor's refresh rate. Or if it's above the "uppers" and it's an override.
+					if((fps > Swift.uppers[i].fps && Swift.uppers[i].fps >= Math.ceil(refreshRate * 0.8)) || (fps > Swift.uppers[i].fps && Swift.uppers[i].overrides)) {
+
+						// if the profile we're about to load isn't already loaded
 						if(i != lastLoadedProfile) {
+
+							// if a command for the profile has been specified in the config, run it
 							if(!Swift.uppers[i].command.isEmpty()) {
 								ClientCommandHandler.instance.executeCommand(player, Swift.uppers[i].command);
 							}
+
+							// if a warning for the profile has been specified in the config, print it out to the chat
 							if(!Swift.uppers[i].warning.isEmpty()) {
 								player.addChatMessage(new ChatComponentText(Swift.uppers[i].warning));
 							}
 						}
+
+						// reset the cooldown timer to the value specified in the config
 						cooldown = Swift.cooldownTicks;
+
+						// set the secondLastLoadedProfile to lastLoadedProfile
+						secondLastLoadedProfile = lastLoadedProfile;
+
+						// set the lastLoadedProfile to the current one
 						lastLoadedProfile = i;
+
+						// done
 						break;
 					}
 				}
 				return;
 			}
 			else {
+				// if we're still cooling down, increment the tick timer
 				ticks++;
 				return;
 			}
